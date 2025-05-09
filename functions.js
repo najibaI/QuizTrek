542e9bb02bfd2543e45807e6d0062571c3888d1e
Najiba I <78580746+najibaI@users.noreply.github.com>
2025-05-09 18:31:39 -0400

    Add files via upload

---
+++ functions.js
@@ -0,0 +1,39 @@
+const toggleButton = document.getElementById('toggle-btn')
+const sidebar = document.getElementById('sidebar')
+
+function toggleSidebar(){
+  sidebar.classList.toggle('close')
+  toggleButton.classList.toggle('rotate')
+
+  closeAllSubMenus()
+}
+
+function toggleSubMenu(button){
+
+  if(!button.nextElementSibling.classList.contains('show')){
+    closeAllSubMenus()
+  }
+
+  button.nextElementSibling.classList.toggle('show')
+  button.classList.toggle('rotate')
+
+  if(sidebar.classList.contains('close')){
+    sidebar.classList.toggle('close')
+    toggleButton.classList.toggle('rotate')
+  }
+}
+
+function toggleLogoutButton(button) {
+    alert("You will be logging out."); // could also show a confirmation message if we want
+    // clear all session data
+    localStorage.removeItem('userToken'); // removing a token from localStorage
+    sessionStorage.removeItem('userInfo'); // remove user info from sessionStorage
+    window.location.href = "login.html"; // redirects user to login page
+}
+
+function closeAllSubMenus(){
+  Array.from(sidebar.getElementsByClassName('show')).forEach(ul => {
+    ul.classList.remove('show')
+    ul.previousElementSibling.classList.remove('rotate')
+  })
+}
