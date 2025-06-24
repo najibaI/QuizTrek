6b993edb892c1b70caaa59aea52f066e1ba887a2
Najiba I <78580746+najibaI@users.noreply.github.com>
2025-06-24 18:19:26 -0400

    Update database.py

--- database.py
+++ database.py
@@ -2,6 +2,6 @@
 from pymongo import MongoClient
 
 # Connects to my MongoDB Cluster
-client = MongoClient("mongodb+srv://quiztrek:quiztrek@quiztrekcluster1.ugjvjbu.mongodb.net/")
+client = MongoClient("mongodb+srv://<db_username>:<db_password>@quiztrekcluster1.ugjvjbu.mongodb.net/")
 
 db = client.quiztrekDB
