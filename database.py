9b44579e34545fd0dfe40753d665574b6ca86657
Najiba I <78580746+najibaI@users.noreply.github.com>
2025-06-27 13:50:58 -0400

    Update database.py

--- database.py
+++ database.py
@@ -2,6 +2,7 @@
 from pymongo import MongoClient
 
 # Connects to my MongoDB Cluster
+# Obiviously, not showing my DB username and password for confidential reasons
 client = MongoClient("mongodb+srv://<db_username>:<db_password>@quiztrekcluster1.ugjvjbu.mongodb.net/")
 
 db = client.quiztrekDB
